c9682ff4c783b5e173088c7cd0a53fc9532db1eb
Aleksandr Kudryavcev <som13x86@gmail.com>
2024-04-19 14:55:24 +0300

    First Stage of Bot Creation

--- database/database.py
+++ database/database.py
@@ -4,7 +4,7 @@ import sqlite3
 async def db_connect():
     table_creation = ("CREATE TABLE IF NOT EXISTS user_requests(user_id TEXT PRIMARY KEY NOT NULL, name TEXT, "
                       "lang_level TEXT,age INT, learn_target TEXT, telephone INT, time_prior TEXT)")
-    with sqlite3.connect("users.db") as connection:
+    with sqlite3.connect("/home/CoolSchool_AIOBot/users.db") as connection:
         cursor = connection.cursor()
         cursor.execute(table_creation)
 
@@ -21,14 +21,14 @@ async def save_user_request(sql_data: [int, dict[str, str | int | bool]]):
         for key2 in sql_data[user_id]:
             records.append(sql_data[user_id][key2])
 
-    with sqlite3.connect("users.db") as connection:
+    with sqlite3.connect("/home/CoolSchool_AIOBot/users.db") as connection:
         cursor = connection.cursor()
         cursor.execute(insert_query, tuple(records))
 
 
 def get_user_request(user_id):
     get_query = (f'SELECT DISTINCT * FROM user_requests WHERE user_id = {user_id}')
-    with sqlite3.connect("users.db") as connection:
+    with sqlite3.connect("/home/CoolSchool_AIOBot/users.db") as connection:
         cursor = connection.cursor()
         user = cursor.execute(get_query).fetchone()
     if user is not None:
